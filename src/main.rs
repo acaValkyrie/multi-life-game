7cc0c487582502b89904d1cb5109ecacf33ecca3
acaValkyrie <acavalkyrie@gmail.com>
2024-03-15 08:55:56 +0900

    セルの配列を定義

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,13 @@ impl Cell {
     fn new(x: u32, y: u32, color: Color, size: u32) -> Cell{
         Cell{object: Object::new(x, y, color, size)}
     }
+    fn set_viability(&mut self, is_alive: bool){
+        if is_alive{
+            self.object.color = Color::RGB(255, 255, 255);
+        }else{
+            self.object.color = Color::RGB(0, 0, 0);
+        }
+    }
     fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>){
         self.object.draw(canvas);
     }
@@ -39,8 +46,15 @@ pub fn main() {
     let (mut canvas, mut event_pump) 
         = sdl_modules::sdl_setup(800, 600);
     
-    let mut cell = Cell::new(100, 100, Color::RGB(255, 255, 255), 100);
-
+    const CELL_BOX_WIDTH_COUNT: usize = 10;
+    let mut cells: [[Cell; CELL_BOX_WIDTH_COUNT]; CELL_BOX_WIDTH_COUNT];
+    for i in 0..CELL_BOX_WIDTH_COUNT{
+        for j in 0..CELL_BOX_WIDTH_COUNT{
+            cells[i][j] = Cell::new(i as u32 * 10, j as u32 * 10, Color::RGB(0, 0, 0), 10);
+            cells[i][j].set_viability(true);
+        }
+    }
+    
     // イベントループ
     'running: loop {
         if sdl_modules::is_end_event(&mut event_pump) { break 'running; }
@@ -48,14 +62,8 @@ pub fn main() {
         // キャンバスの初期化
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-
-        let state = &event_pump.keyboard_state();
-        if state.is_scancode_pressed(Scancode::Up){    cell.object.y -= 5; }
-        if state.is_scancode_pressed(Scancode::Down){  cell.object.y += 5; }
-        if state.is_scancode_pressed(Scancode::Left){  cell.object.x -= 5; }
-        if state.is_scancode_pressed(Scancode::Right){ cell.object.x += 5; }
         
-        cell.draw(&mut canvas);
+        // cell.draw(&mut canvas);
 
         canvas.present();
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
