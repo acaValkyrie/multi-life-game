59734d91736b98146a1e6c6f1f1988da3aaa7671
acaValkyrie <acavalkyrie@gmail.com>
2024-03-15 09:15:15 +0900

    cellの生死を記録するbool配列を作って、セルの描画をtrueのときにだけするように変更

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,6 @@ impl Cell {
     fn new(x: u32, y: u32, color: Color, size: u32) -> Cell{
         Cell{object: Object::new(x, y, color, size)}
     }
-    fn set_viability(&mut self, is_alive: bool){
-        if is_alive{
-            self.object.color = Color::RGB(255, 255, 255);
-        }else{
-            self.object.color = Color::RGB(0, 0, 0);
-        }
-    }
     fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>){
         self.object.draw(canvas);
     }
@@ -47,11 +40,10 @@ pub fn main() {
         = sdl_modules::sdl_setup(800, 600);
     
     const CELL_BOX_WIDTH_COUNT: usize = 10;
-    let mut cells: [[Cell; CELL_BOX_WIDTH_COUNT]; CELL_BOX_WIDTH_COUNT];
+    let mut cell_viabilities: [[bool; CELL_BOX_WIDTH_COUNT]; CELL_BOX_WIDTH_COUNT] = [[false; CELL_BOX_WIDTH_COUNT]; CELL_BOX_WIDTH_COUNT];
     for i in 0..CELL_BOX_WIDTH_COUNT{
         for j in 0..CELL_BOX_WIDTH_COUNT{
-            cells[i][j] = Cell::new(i as u32 * 10, j as u32 * 10, Color::RGB(0, 0, 0), 10);
-            cells[i][j].set_viability(true);
+            cell_viabilities[i][j] = true;
         }
     }
     
@@ -63,7 +55,14 @@ pub fn main() {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
         
-        // cell.draw(&mut canvas);
+        for i in 0..CELL_BOX_WIDTH_COUNT{
+            for j in 0..CELL_BOX_WIDTH_COUNT{
+                if cell_viabilities[i][j] == true{
+                    let cell = Cell::new((i * 50) as u32, (j * 50) as u32, Color::RGB(255, 255, 255), 50);
+                    cell.draw(&mut canvas);
+                }
+            }
+        }
 
         canvas.present();
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
